73aa55badca7269ce00ba0e91381bc5e377753c4
PedroIghor <pedro_pucci01@hotmail.com>
2025-09-07 16:08:43 -0300

    configuração inicial para o projeto rodar

---
+++ myApp/App.tsx
@@ -0,0 +1,18 @@
+import { SafeAreaView, Text, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>TrainMasterApp</Text>
+      <Text style={styles.subtitle}>Está rodando 🎉</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#F0F4F3" },
+  title: { fontSize: 20, fontWeight: "700", marginBottom: 8 },
+  subtitle: { fontSize: 14, opacity: 0.7 },
+});
